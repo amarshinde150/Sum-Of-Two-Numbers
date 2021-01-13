aaa14504f3d1203c5e94bef4f03a35711a545be0
amarshinde150 <amarshinde150@gmail.com>
2021-01-13 14:56:41 +0530

    added difference also

--- SumOfTwoNumbers.c
+++ SumOfTwoNumbers.c
@@ -6,6 +6,7 @@ int main()
   printf("Enter two numbers\n");
   scanf("%d%d",&a,&b);
   // sum is a+b
-  printf("Sum of two numbers %d and %d is %d",a,b,a+b);
+  printf("Sum of two numbers %d and %d is %d\n",a,b,a+b);
+  printf("Difference of two number %d and %d is %d",a,b,a-b);
   return 0;
 }
