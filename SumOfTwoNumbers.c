bbacd1d9d2336aa284aed8925d878713b00dd8e3
amarshinde150 <amarshinde150@gmail.com>
2021-01-13 14:51:00 +0530

    Added comments in Code

--- SumOfTwoNumbers.c
+++ SumOfTwoNumbers.c
@@ -2,8 +2,10 @@
 int main()
 {
   int a,b;
+  //Take Input From user
   printf("Enter two numbers\n");
   scanf("%d%d",&a,&b);
+  // sum is a+b
   printf("Sum of two numbers %d and %d is %d",a,b,a+b);
   return 0;
 }
